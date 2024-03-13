0bca970e2f1260ef6104df0b7801676271df435e
Voronina Maria <m.voronina0803@gmail.com>
2024-03-13 14:55:47 +0200

    delete 0.2

--- app/connectors/brands/Monobrand/constants.js
+++
@@ -1,9 +0,0 @@
-export const PATHS = {
-    createUser: "auth/create",
-    userBalance: "balance",
-    topup: "balance/refill",
-    topupHistory: "balance/transactions/1687966489",
-    prices: "card/prices",
-    history: "card/transactions/1687966489",
-    getQr: "card/qr",
-};
