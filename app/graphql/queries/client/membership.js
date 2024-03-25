f27f794e0a8960e9899ff2a8484dc24682319cb1
Voronina Maria <m.voronina0803@gmail.com>
2024-03-25 16:30:20 +0200

    Added query

--- app/graphql/queries/client/membership.js
+++ app/graphql/queries/client/membership.js
@@ -1,11 +1,12 @@
 import { GraphQLList, GraphQLError } from "graphql";
 import { Database } from "@local/lib/index.js";
 import { Membership as MembershipType } from "@local/graphql/types/index.js";
+import { GraphQLID, GraphQLNonNull } from "graphql/index.js";
 
-const membership = {
+const ActiveMembership = {
   type: MembershipType,
-  resolve: (_, __, { client }) => {
-    const membershipRes = Database("memberships")
+  resolve: async (_, __, { client }) => {
+    const membershipRes = await "memberships"
       .where({
         id: client.membership_id,
       })
@@ -36,7 +37,43 @@ const membership = {
   },
 };
 
-const memberships = {
+const Membership = {
+  type: MembershipType,
+  args: { id: { type: new GraphQLNonNull(GraphQLID) } },
+  resolve: async (_, { id }, { client }) => {
+    const membershipRes = await Database("memberships")
+      .where({
+        id,
+        merchant_id: client.merchant_id,
+      })
+      .first()
+      .then(async (membership) => {
+        membership.abilities = await Database("abilities")
+          .select([
+            "id",
+            "name",
+            "description",
+            "regular_price",
+            "discount_price",
+          ])
+          .where({
+            membership_id: membership.id,
+          })
+          .catch(() => {
+            throw new GraphQLError("Forbidden");
+          });
+        return membership;
+      })
+
+      .catch(() => {
+        throw new GraphQLError("Forbidden");
+      });
+
+    return membershipRes;
+  },
+};
+
+const allMemberships = {
   type: new GraphQLList(MembershipType),
   resolve: (_, __, { client }) =>
     Database("memberships")
@@ -63,4 +100,4 @@ const memberships = {
       }),
 };
 
-export { membership, memberships };
+export default { ActiveMembership, allMemberships, Membership };
