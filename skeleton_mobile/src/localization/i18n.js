23ed54f1666682c12c2d07c0f24beac16554d230
Aliona <alionkakhuda28@gmail.com>
2024-04-01 14:42:56 +0300

    Added localization

--- skeleton_mobile/src/localization/i18n.js
+++ skeleton_mobile/src/localization/i18n.js
@@ -3,13 +3,16 @@ import { initReactI18next } from "react-i18next";
 import "intl-pluralrules";
 
 import ukrainian from "../locale/uk.json";
+import english from "../locale/en.json";
 
 i18n.use(initReactI18next).init({
   compatibilityJSON: "v3",
   lng: "uk",
+  fallbackLng: "en",
 
   resources: {
     uk: ukrainian,
+    en: english,
   },
   interpolation: {
     escapeValue: false,
